27d8a2953eb317d99d153d8e9c73d725716d729e
Daniel Chen <chendaniely@gmail.com>
2024-03-26 08:35:27 -0700

    remove all extensions

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@ author = u"Daniel Chen"
 # ones.
 extensions = [
     #"myst_nb",
-    "autoapi.extension",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode"
+    #"autoapi.extension",
+    #"sphinx.ext.napoleon",
+    #"sphinx.ext.viewcode"
 ]
 autoapi_dirs = ["../src"]
 
